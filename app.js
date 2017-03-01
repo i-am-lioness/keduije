65994ced1ebfc093dddee092f328e1bfcfdb1914
nnennaude <nnenna.udegbunam@gmail.com>
2017-03-01 10:27:13 -0500

    fixing port for heroku

--- app.js
+++ app.js
@@ -77,8 +77,9 @@ MongoClient.connect(url, function(err, db) {
   console.log("Connected successfully to server");
   database = db;
 
-  app.listen(3000, function () {
-    console.log('Example app listening on port 3000!')
+  var port = (process.env.PORT || 3000);
+  app.listen(port, function () {
+    console.log('Example app listening on port ' + port + "!");
   })
 
 });
