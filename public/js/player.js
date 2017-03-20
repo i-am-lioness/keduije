cd16abb049f74eaa5288c4ca109a09e7aba2ddea
nnennaude <nnenna.udegbunam@gmail.com>
2017-03-19 22:41:00 -0400

    fixing youtube api loading bug
    now loads youtube api only after sure that callback is set
    +some code clean up

--- public/js/player.js
+++ public/js/player.js
@@ -1,28 +1,27 @@
 
 var KeduIje = (function(ki){
 
-var songID = null;
+  var songID = null;
 
-function loadLyrics(cb){
-  $.get("/lyrics/"+songID, cb);
-}
+  function loadLyrics(cb){
+    $.get("/lyrics/"+songID, cb);
+  }
 
-function addLyric(newLyric, cb){
+  function addLyric(newLyric, cb){
+    $.post("/lyrics/"+songID+"/addline", newLyric, cb);
+  }
 
-  $.post("/lyrics/"+songID+"/addline", newLyric, cb);
-}
-
-function updateLyric(oldLyricObj, newLyricObj, cb){
+  function updateLyric(oldLyricObj, newLyricObj, cb){
 
     var postData = {
       new: newLyricObj,
       original: oldLyricObj
     };
 
-  //todo: postdata should be validated
-  $.post("/lyrics/"+songID+"/editline/"+oldLyricObj.id, postData, cb);
+    //todo: postdata should be validated
+    $.post("/lyrics/"+songID+"/editline/"+oldLyricObj.id, postData, cb);
 
-}
+  }
 
   ki.init = function (_songID){
     songID = _songID;
@@ -44,12 +43,11 @@ URGENT: fix edit lyric bug
 --look at http://localhost:3000/music/id/kbVhw7muUnY
 --copy of ids 6 & 7
 
---explore integrating LyricDisplay component into MediaPlayer
 -fully integrate audio
+--make smoother highlting flow
 --new music form
 --re-design data schema
---implement controls
---move timer controlls to hook
+
 
 Backend
 --remove "isHeading" field form lyrics
@@ -57,13 +55,9 @@ Backend
 -UI design
 --design control
 
--move pug rednering of lyric display to client side
---remove obsolete template
-
 -bug: after login, goes to http://localhost:3000/music/id/h01_dEXLqsk#_=_ (achikolo)
 
 -error handling
---make sure that youtube api loaded on time
 
 -log visitors
 
