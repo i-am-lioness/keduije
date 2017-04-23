d8c79605ec037fd7f6efd7bcca3f30f386d26d75
nnennaude <nnenna.udegbunam@gmail.com>
2017-04-23 05:46:14 -0400

    [non source code] script to update db objects to match new data model

--- script.js
+++ script.js
@@ -1,164 +1,90 @@
-var MongoClient = require('mongodb').MongoClient
-  , assert = require('assert');
+/* eslint no-console: 0 */
+const MongoClient = require('mongodb').MongoClient;
+const ObjectId = require('mongodb').ObjectId;
 
+const url = 'mongodb://igbo:igbo@ds151070.mlab.com:51070/igbo_dev';
+let db;
 
-// Connection URL
-var url = 'mongodb://localhost:27017/igbo';
-var url = 'mongodb://igbo:igbo@ds111940.mlab.com:11940/igbo';
-var database;
-
-
-function processLyrics(lyrics){
-  var lastEndTime = 0;
-  for (var i=0; i < lyrics.length; i ++){
-    lyrics[i].startTime=lastEndTime;
-    lastEndTime = lyrics[i].time;
-    lyrics[i].endTime = lastEndTime;
-    lyrics[i].id = i;
-    lyrics[i].deleted = false;
-    lyrics[i].isHeading = false;
-  }
-
-  console.log("song:");
-  //console.log(lyrics);
+function upgradeLines() {
+  db.collection('lines').find().forEach((line) => {
+    console.log(line.text);
+    line.media = ObjectId(line.mediaID);
+    line.changeset = ObjectId(line.changeset);
+    // delete line.mediaID;
+    db.collection('lines').save(line);
+  }, () => {
+    db.close();
+    process.exit(0);
+  });
 }
 
+function deleteMediaProp() {
+  db.collection('lines').find().forEach((line) => {
+    console.log(line.text);
+    delete line.mediaID;
+    db.collection('lines').save(line);
+  }, () => {
+    db.close();
+    process.exit(0);
+  });
+}
 
-function convertNumbers(lyrics){
-  for (var i=0; i < lyrics.length; i++){
-    lyrics[i].startTime=parseInt(lyrics[i].startTime);
-    lyrics[i].endTime = parseInt(lyrics[i].endTime);
-    delete lyrics[i].time;
-  }
+function upgradeChangesets() {
+  db.collection('changesets').find().forEach((cs) => {
+    //console.log(cs.text);
+    cs.media = ObjectId(cs.mediaID);
+    delete cs.mediaID;
+    db.collection('changesets').save(cs);
+  }, () => {
+    db.close();
+    process.exit(0);
+  });
+}
 
-  console.log("song:");
-  //console.log(lyrics);
+function upgradeMedia() {
+  db.collection('media').find().forEach((media) => {
+    console.log(media.title);
+    if (media.changeset) {
+      media.changeset = ObjectId(media.changeset);
+      db.collection('media').save(media);
+    }
+  });
+}
+
+function upgradeRevisions() {
+  db.collection('revisions').find().forEach((r) => {
+    if (r.original) {
+      const original = r.original;
+      console.log(original.text);
+      if (original.creator) {
+        original.creator = ObjectId(original.creator);
+      }
+      if (original.mediaID) {
+        original.media = ObjectId(original.mediaID);
+        delete original.mediaID;
+      }
+      if (original._id) {
+        original._id = ObjectId(original._id);
+      }
+    }
+    if (r.mediaID) {
+      r.media = ObjectId(r.mediaID);
+      delete r.mediaID;
+    }
+    if (r.changeset) {
+      r.changeset = ObjectId(r.changeset);
+    }
+
+    db.collection('revisions').save(r);
+  });
 }
 
 
 // Use connect method to connect to the server
-MongoClient.connect(url, function(err, db) {
-  assert.equal(null, err);
-  console.log("Connected successfully to server");
-  database = db;
+MongoClient.connect(url, (err, _db) => {
+  console.log('Connected successfully to server');
+  db = _db;
 
-  database.collection('lyrics').find().toArray(function(err, songs) {
-    songs.forEach(function(song){
-      console.log(song);
-      //convertNumbers(song.lyrics);
-
-      //updateInDB(song);
-
-    })
-  });
+  upgradeRevisions();
 });
 
-
-function updateInDB(song){
-  database.collection('lyrics').update(
-    {_id: song._id},
-    song,
-    function(err, n, object) {
-      console.log("update completed");
-      if (err){
-          console.warn(err.message);  // returns error if no matching object found
-      }else{
-          console.dir(object);
-      }
-    });
-
-}
-
-/*
-app.get('/lyrics/:videoID', function (req, res) {
-
-  database.collection('lyrics')
-    .find({ videoID: req.params.videoID } )
-    .nextObject(function(err, obj) {
-    console.log(obj)
-
-    var result = (obj) ? obj.lyrics : [];
-    res.send(result);
-  })
-});
-
-app.get('/music/:videoID', function (req, res) {
-
-  database.collection('lyrics')
-    .find({ videoID: req.params.videoID } )
-    .nextObject(function(err, obj) {
-      console.log(obj)
-
-      var lyrics = (obj) ? obj.lyrics : [];
-
-      res.render('player', {
-        title: "hello",
-        videoID: req.params.videoID
-      });
-  })
-});
-
-
-app.post('/', function (req, res) {
-
-  console.log(req.body);
-  var obj = req.body;
-  //res.send(obj);
-
-  database.collection('lyrics').update(
-     { videoID: obj.videoID } ,
-     { $set: { lyrics: obj.lyrics } },
-     { upsert: true },
-     function(err, result) {
-       assert.equal(err, null);
-       res.send(result);
-     }
-  );
-
-});
-
-app.get('/test', function (req, res) {
-
-  var obj = {videoID: "hello", lyrics: [1, 2, 3]};
-
-  database.collection('lyrics').update(
-     { videoID: obj.videoID } ,
-     { $set: { lyrics: obj.lyrics } },
-     { upsert: true },
-     function(err, result) {
-       assert.equal(err, null);
-       res.send(result);
-     }
-  );
-
-});
-*/
-
-/*
-var insertDocuments = function(db, callback) {
-  // Get the documents collection
-  var collection = db.collection('documents');
-  // Insert some documents
-  collection.insertMany([
-    {a : 1}, {a : 2}, {a : 3}
-  ], function(err, result) {
-    assert.equal(err, null);
-    assert.equal(3, result.result.n);
-    assert.equal(3, result.ops.length);
-    console.log("Inserted 3 documents into the collection");
-    callback(result);
-  });
-}
-
-var findDocuments = function(db, callback) {
-  // Get the documents collection
-  var collection = db.collection('documents');
-  // Find some documents
-  collection.find({}).toArray(function(err, docs) {
-    assert.equal(err, null);
-    console.log("Found the following records");
-    console.log(docs)
-    callback(docs);
-  });
-}
-*/
