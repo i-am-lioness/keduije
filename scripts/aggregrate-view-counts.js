5a90901d9ec3dd4008cc158789670a80e3de34d3
nnennaude <nnenna.udegbunam@gmail.com>
2017-04-28 05:43:28 -0400

    refactored aggregate-view-counts.js

--- scripts/aggregrate-view-counts.js
+++ scripts/aggregrate-view-counts.js
@@ -1,43 +1,38 @@
-require('dotenv').config()
-var MongoClient = require('mongodb').MongoClient;
+require('dotenv').config();
+const MongoClient = require('mongodb').MongoClient;
 
-var db;
-var res;
+let db;
 
-function forSong(err,song){
-  if(song==null){
-    db.close();
-    return;
-  }
+function forSong(song) {
   console.log(song.title);
 
   song.history = song.history || [];
-  song.history.push({asOf: new Date(), views: song.views});
+  song.history.push({ asOf: new Date(), views: song.views });
 
 
-  var weeklyTotal = 0;
-  song.history.forEach((el)=>{
-    //todo: actually check the dates to make sure they are within the period in question
+  let weeklyTotal = 0;
+  song.history.forEach((el) => {
+    // todo: actually check the dates to make sure they are within the period in question
     weeklyTotal += el.views;
   });
 
-  if(song.history.length > 4) //will eventually be removed
+  // will eventually be removed
+  if (song.history.length > 4) {
     song.history.shift();
+  }
 
   song.totalViews = weeklyTotal;
   song.views = 0;
 
-  db.collection('lyrics').save(song, ()=>{
-    res.nextObject(forSong);
-  });
+  db.collection('media').save(song);
 }
 
-MongoClient.connect(process.env.DB_URL, function(err, _db) {
-  console.log("Connected successfully to server");
-  db=_db;
+MongoClient.connect(process.env.DB_URL, (err, _db) => {
+  console.log('Connected successfully to server');
+  db = _db;
 
-  res = db.collection('lyrics').find();
-
-  res.nextObject(forSong);
-
+  db.collection('media').find().forEach(forSong, () => {
+    db.close();
+    process.exit();
+  });
 });
