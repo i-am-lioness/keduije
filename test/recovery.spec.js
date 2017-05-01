c6178e046850b80ec744621e1cb2f454b75488c5
nnennaude <nnenna.udegbunam@gmail.com>
2017-05-01 06:04:05 -0400

    [untested] script to aggregate revisions into changesets

---
+++ test/recovery.spec.js
@@ -0,0 +1,10 @@
+/* eslint-env mocha */
+// import { expect } from 'chai';
+// import TestDB from './utils/db';
+
+
+describe('app.js', () => {
+  it('clears failed revisions');
+
+  it('cleans up sessions');
+});
